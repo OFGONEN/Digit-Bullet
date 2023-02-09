6aefac0750c812fd81e1b6986f4328e183153397
ofgonen-home <ofgonen@gmail.com>
2023-02-09 14:23:59 +0300

    ActorNumber: Movement

--- Assets/Script/ActorNumber.cs
+++ Assets/Script/ActorNumber.cs
@@ -12,8 +12,13 @@ public class ActorNumber : MonoBehaviour
   [ Title( "Setup" ) ]
     [ SerializeField ] float number_value;
 
+  [ Title( "Components" ) ]
+	[ SerializeField ] Rigidbody _rigidbody;
+
 	Vector3 movement_direction;
 	float number_value_current;
+
+	UnityMessage onFixedUpdate;
 #endregion
 
 #region Properties
@@ -24,6 +29,13 @@ public class ActorNumber : MonoBehaviour
     void Awake()
     {
 		number_value_current = number_value;
+
+		EmptyDelegates();
+	}
+
+	private void FixedUpdate()
+	{
+		onFixedUpdate();
 	}
 #endregion
 
@@ -33,6 +45,14 @@ public class ActorNumber : MonoBehaviour
 		gameObject.SetActive( false );
 	}
 
+	[ Button() ]
+	public void StartMovement( Vector3 direction )
+	{
+		movement_direction = direction;
+
+		onFixedUpdate = OnMovement;
+	}
+
 	public void DoRicochet( Vector3 normal )
 	{
 		movement_direction = Vector3.Reflect( movement_direction, normal );
@@ -40,6 +60,18 @@ public class ActorNumber : MonoBehaviour
 #endregion
 
 #region Implementation
+	void OnMovement()
+	{
+		var position       = _rigidbody.position;
+		var targetPosition = _rigidbody.position + movement_direction * Time.fixedDeltaTime * GameSettings.Instance.actor_movement_speed;
+
+		_rigidbody.MovePosition( targetPosition );
+	}
+
+	void EmptyDelegates()
+	{
+		onFixedUpdate = Extensions.EmptyMethod;
+	}
 #endregion
 
 #region Editor Only
