9c4ed2dc0698cc1cd27a0b31bc2222ce6c0dad29
ofgonen-home <ofgonen@gmail.com>
2023-02-10 16:50:04 +0300

    ActorNumber: Movement

--- Assets/Script/ActorNumber.cs
+++ Assets/Script/ActorNumber.cs
@@ -3,6 +3,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEditor;
 using FFStudio;
 using Sirenix.OdinInspector;
 
@@ -15,8 +16,11 @@ public class ActorNumber : MonoBehaviour, ISafetyCollectable
   [ Title( "Components" ) ]
 	[ SerializeField ] Rigidbody _rigidbody;
 
-	Vector3 movement_direction;
+	Vector3 movement_target_direction;
+	Vector3 movement_target_position;
+	ICustomNormal movement_target;
 	float number_value_current;
+	int layerMask;
 
 	UnityMessage onFixedUpdate;
 #endregion
@@ -26,9 +30,15 @@ public class ActorNumber : MonoBehaviour, ISafetyCollectable
 #endregion
 
 #region Unity API
+	private void OnDisable()
+	{
+		EmptyDelegates();
+	}
+
     void Awake()
     {
 		number_value_current = number_value;
+		layerMask            = 1 << GameSettings.Instance.trajectory_layer;
 
 		EmptyDelegates();
 	}
@@ -48,9 +58,9 @@ public class ActorNumber : MonoBehaviour, ISafetyCollectable
 	[ Button() ]
 	public void StartMovement( Vector3 direction )
 	{
-		movement_direction = direction;
+		movement_target_direction = direction;
 
-		onFixedUpdate = OnMovement;
+		FindMovementTargetPosition();
 	}
 
 	public void OnSafetyNetTrigger() 
@@ -60,12 +70,51 @@ public class ActorNumber : MonoBehaviour, ISafetyCollectable
 #endregion
 
 #region Implementation
-	void OnMovement()
+	void OnMovementFree()
 	{
-		var position       = _rigidbody.position;
-		var targetPosition = _rigidbody.position + movement_direction * Time.fixedDeltaTime * GameSettings.Instance.actor_movement_speed;
+		var position     = _rigidbody.position;
+		var nextPosition = Vector3.MoveTowards( position, movement_target_position, Time.deltaTime * GameSettings.Instance.actor_movement_speed );
 
-		_rigidbody.MovePosition( targetPosition );
+		_rigidbody.MovePosition( nextPosition );
+	}
+
+	void OnMovementTarget()
+	{
+		var position = _rigidbody.position;
+		var nextPosition = Vector3.MoveTowards( position, movement_target_position, Time.deltaTime * GameSettings.Instance.actor_movement_speed );
+
+		_rigidbody.MovePosition( nextPosition );
+
+		if( nextPosition == position )
+		{
+			movement_target_direction = Vector3.Reflect( 
+				movement_target_direction, 
+				movement_target.GetNormal( movement_target_position ) );
+
+			FindMovementTargetPosition();
+		}
+	}
+
+	void FindMovementTargetPosition()
+	{
+		RaycastHit raycastHit;
+		var position = transform.position;
+
+		var isHit = Physics.Raycast( position, movement_target_direction, out raycastHit,
+			GameSettings.Instance.actor_movement_delta_max,
+			layerMask );
+
+		if( isHit )
+		{
+			movement_target_position = raycastHit.point;
+			movement_target          = raycastHit.collider.GetComponent< ICustomNormal >();
+			onFixedUpdate                 = OnMovementTarget;
+		}
+		else
+		{
+			movement_target_position = position + movement_target_direction * GameSettings.Instance.actor_movement_delta_max;
+			onFixedUpdate                 = OnMovementFree;
+		}
 	}
 
 	void EmptyDelegates()
@@ -76,6 +125,12 @@ public class ActorNumber : MonoBehaviour, ISafetyCollectable
 
 #region Editor Only
 #if UNITY_EDITOR
+	private void OnDrawGizmos()
+	{
+		if( !Application.isPlaying ) return;
+
+		Handles.DrawLine( transform.position, movement_target_position );
+	}
 #endif
 #endregion
 }
