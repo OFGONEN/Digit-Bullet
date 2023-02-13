228a203fe0da73b7b8403c5533a04e30de0eda10
ofgonen-home <ofgonen@gmail.com>
2023-02-13 12:12:31 +0300

    Implement DigitDisplayer

---
+++ Assets/Script/DigitDisplayer.cs
@@ -0,0 +1,35 @@
+/* Created by and for usage of FF Studios (2021). */
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DigitDisplayer : MonoBehaviour
+{
+#region Fields
+    [ SerializeField ] MeshFilter _meshFilter;
+    [ SerializeField ] MeshRenderer _meshRenderer;
+#endregion
+
+#region Properties
+#endregion
+
+#region Unity API
+#endregion
+
+#region API
+    public void UpdateVisual( NumberDisplayData data, Material material )
+    {
+		_meshFilter.mesh       = data.mesh;
+		_meshRenderer.material = material;
+	}
+#endregion
+
+#region Implementation
+#endregion
+
+#region Editor Only
+#if UNITY_EDITOR
+#endif
+#endregion
+}
