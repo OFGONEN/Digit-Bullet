8da3f8bc38113501cb9b4d2dbf37e7e6c5e5ea2c
ofgonen-home <ofgonen@gmail.com>
2023-02-13 12:14:48 +0300

    AssetManager: Init pools

--- Assets/Script/FFStudio/Manager/AssetManager.cs
+++ Assets/Script/FFStudio/Manager/AssetManager.cs
@@ -23,6 +23,7 @@ namespace FFStudio
 	[ Title( "Pool" ) ]
 		[ SerializeField ] Pool_UIPopUpText pool_UIPopUpText;
 		[ SerializeField ] PoolActorNumber pool_number_actor;
+		[ SerializeField ] PoolDigitDisplayer pool_digit_displayer;
 #endregion
 
 #region UnityAPI
@@ -37,6 +38,8 @@ namespace FFStudio
 
 			pool_UIPopUpText.InitPool( transform, false );
 			pool_number_actor.InitPool( transform, false );
+			pool_digit_displayer.InitPool( transform, false );
+
 			onAwakeEvent.Invoke();
 		}
 
